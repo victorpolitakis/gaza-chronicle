ec907f6cedff20cb869cdc1f74bddbeb80ae8586
vpolitakis001 <victor.politakis@pwc.com>
2026-01-18 09:55:04 +0200

    Enhance map realism with Esri Topo tiles

--- app.js
+++ app.js
@@ -407,10 +407,9 @@ function initMap() {
         zoomControl: false // Custom placement below
     });
 
-    // Add CartoDB Voyager tiles (clean, professional style)
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
+    // Add Esri World Topo Map tiles (More realistic, traditional map style)
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
         maxZoom: 19
     }).addTo(map);
 
